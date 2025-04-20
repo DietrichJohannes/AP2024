708f9eddcc7340f874c8b191d24644cca4ad61d9
Johannes Dietrich <Johannes Dietrich>
2025-04-20 20:48:53 +0200

    Removed Installer Folder

---
+++ AP2024/AbsenceControllerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AP2024
+{
+    public static class AbsenceControllerExtensions
+    {
+        public static Task InvokeAsync(this Control control, Action action)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            control.BeginInvoke(new MethodInvoker(() =>
+            {
+                try
+                {
+                    action();
+                    tcs.SetResult(null);
+                }
+                catch (Exception ex)
+                {
+                    tcs.SetException(ex);
+                }
+            }));
+            return tcs.Task;
+        }
+    }
+}
